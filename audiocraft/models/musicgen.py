3e71455b5a6b0f1b40133d3cb69f489c87378a4e
Michael Hedman <craftmine1000@gmail.com>
2023-06-19 00:37:11 +0200

    testing

--- audiocraft/models/musicgen.py
+++ audiocraft/models/musicgen.py
@@ -363,7 +363,7 @@ class MusicGen:
         for melody in melody_wavs:
             tmp_mld = []
             for i in range(0, melody.shape[-1], re_prompt_rate_sr):
-                cut = melody[:,max(0, i - re_prompt_mod_sr) :]
+                cut = melody[:, i : i + re_prompt_rate_sr]
                 missing = re_prompt_mod_sr - cut.shape[-1]
                 cut = torch.nn.functional.pad(cut, (missing, 0))
                 tmp_mld.append(cut)
